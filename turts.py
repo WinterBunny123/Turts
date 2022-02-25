0ca39c4b66250c236060fc023fe408145effce20
Winter Bunny <100171761+WinterBunny123@users.noreply.github.com>
2022-02-24 19:09:18 -0500

    Create turts.py

---
+++ turts.py
@@ -0,0 +1,47 @@
+import turtle
+
+s=turtle.Screen()
+t=turtle.Turtle()
+
+t.speed(1000)
+
+def square():
+    for i in range(4):
+        t.forward(250)
+        t.right(135)
+
+
+
+for i in range(234):
+    square()
+    t.left(1)
+
+
+    #import turtle
+
+#s=turtle.Screen()
+#t=turtle.Turtle()
+
+#t.speed(1000)
+
+#def square():
+    #for i in range(4):
+        #t.forward(250)
+        #t.right(135)
+
+
+
+#for i in range(234):
+    #square()
+    #t.left(1)
+
+    def square():
+        for i in range(4):
+            t.forward(350)
+        t.right(135)
+
+
+
+for i in range(234):
+    square()
+    t.left(1)
